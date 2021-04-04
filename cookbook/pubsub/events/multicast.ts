adb61b34c4ace01004aba96dce6c80b4e25f239d
KubeMQ <lior.nabat@kubemq.io>
2021-04-04 14:21:46 +0300

    add promise to all functions

--- cookbook/pubsub/events/multicast.ts
+++ cookbook/pubsub/events/multicast.ts
@@ -5,19 +5,44 @@ const opts: Config = {
   clientId: Utils.uuid(),
 };
 const eventsClient = new EventsClient(opts);
-const subscriberA = eventsClient.subscribe({
-  channel: 'events.A',
-});
-subscriberA.onEvent.on((event) => console.log('SubscriberA', event));
-subscriberA.onError.on((error) => console.error('SubscriberA', error));
-subscriberA.onStateChanged.on((state) => console.log('SubscriberA', state));
+(async () => {
+  await eventsClient
+    .subscribe({
+      channel: 'events.A',
+    })
+    .then((subscriber) => {
+      subscriber.onEvent.on((event) => console.log('SubscriberA', event));
+      subscriber.onError.on((error) => {
+        console.error('SubscriberA', error);
+      });
+      subscriber.onStateChanged.on((state) =>
+        console.log('SubscriberA', state),
+      );
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+})();
 
-const subscriberB = eventsClient.subscribe({
-  channel: 'events.B',
-});
-subscriberB.onEvent.on((event) => console.log('SubscriberB', event));
-subscriberB.onError.on((error) => console.error('SubscriberB', error));
-subscriberB.onStateChanged.on((state) => console.log('SubscriberB', state));
+(async () => {
+  await eventsClient
+    .subscribe({
+      channel: 'events.B',
+    })
+    .then((subscriber) => {
+      subscriber.onEvent.on((event) => console.log('SubscriberB', event));
+      subscriber.onError.on((error) => {
+        console.error('SubscriberB', error);
+      });
+      subscriber.onStateChanged.on((state) =>
+        console.log('SubscriberB', state),
+      );
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+})();
+
 setTimeout(() => {
   for (let i = 0; i < 20; i++) {
     eventsClient
@@ -27,6 +52,5 @@ setTimeout(() => {
 }, 2000);
 
 setTimeout(() => {
-  subscriberA.cancel();
-  subscriberB.cancel();
+  eventsClient.close();
 }, 4000);
