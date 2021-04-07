d9581de83732317de435e4b07bdf19e08da0f83a
KubeMQ <lior.nabat@kubemq.io>
2021-04-07 06:21:23 +0300

    add commands/queries examples

--- cookbook/rpc/commnds/send_receive.ts
+++ cookbook/rpc/commnds/send_receive.ts
@@ -1,68 +1,73 @@
 import { CommandsClient, Config, Utils } from '../../../src';
 
-function main() {
+async function main() {
   const opts: Config = {
     address: 'localhost:50000',
     clientId: 'asd',
   };
   const commandsClient = new CommandsClient(opts);
-  const subscriberA = commandsClient.subscribe({
-    channel: 'commands',
-    group: 'g1',
-    clientId: 'command-subscriberA',
-  });
-  const subscriberB = commandsClient.subscribe({
-    channel: 'commands',
-    group: 'g1',
-    clientId: 'command-subscriberB',
-  });
-
-  subscriberA.onCommand.on((command) => {
-    console.log('SubscriberA', command);
+  await commandsClient.subscribe(
+    {
+      channel: 'commands',
+      group: 'g1',
+      clientId: 'command-subscriberA',
+    },
+    (err, msg) => {
+      if (err) {
+        console.error('command-subscriberA', err);
+        return;
+      }
+      if (msg) {
+        console.log('SubscriberA', msg);
+        commandsClient
+          .response({
+            executed: true,
+            error: '',
+            replyChannel: msg.replyChannel,
+            clientId: 'command-responseA',
+            timestamp: Date.now(),
+            id: msg.id,
+          })
+          .catch((reason) => console.log(reason));
+      }
+    },
+  );
+  await commandsClient.subscribe(
+    {
+      channel: 'commands',
+      group: 'g1',
+      clientId: 'command-subscriberB',
+    },
+    (err, msg) => {
+      if (err) {
+        console.error('command-subscriberB', err);
+        return;
+      }
+      if (msg) {
+        console.log('SubscriberB', msg);
+        commandsClient
+          .response({
+            executed: true,
+            error: '',
+            replyChannel: msg.replyChannel,
+            clientId: 'command-responseB',
+            timestamp: Date.now(),
+            id: msg.id,
+          })
+          .catch((reason) => console.log(reason));
+      }
+    },
+  );
+  await new Promise((r) => setTimeout(r, 2000));
+  for (let i = 0; i < 10; i++) {
     commandsClient
-      .response({
-        executed: true,
-        error: '',
-        replyChannel: command.replyChannel,
-        clientId: 'command-responseA',
-        timestamp: Date.now(),
-        id: command.id,
+      .send({
+        channel: 'commands',
+        body: Utils.stringToBytes('data'),
+        timeout: 10000,
+        clientId: 'command-sender',
       })
-      .catch((reason) => console.log(reason));
-  });
-  subscriberA.onError.on((error) => console.error('SubscriberA', error));
-  subscriberA.onStateChanged.on((state) => console.log('SubscriberA', state));
-
-  subscriberB.onCommand.on((command) => {
-    console.log('SubscriberB', command);
-    commandsClient
-      .response({
-        executed: true,
-        error: '',
-        replyChannel: command.replyChannel,
-        clientId: 'command-responseB',
-        timestamp: Date.now(),
-        id: command.id,
-      })
-      .catch((reason) => console.log(reason));
-  });
-  subscriberB.onError.on((error) => console.error('SubscriberB', error));
-  subscriberB.onStateChanged.on((state) => console.log('SubscriberB', state));
-
-  setTimeout(() => {
-    for (let i = 0; i < 20; i++) {
-      commandsClient
-        .send({
-          channel: 'commands',
-          body: Utils.stringToBytes('data'),
-          timeout: 10000,
-          clientId: 'command-sender',
-        })
-        .catch((reason) => console.error(reason));
-    }
-  }, 2000);
-  setTimeout(() => {
-    commandsClient.close();
-  }, 4000);
+      .catch((reason) => console.error(reason));
+  }
 }
 main();
--- cookbook/rpc/queries/send_receive.ts
+++ cookbook/rpc/queries/send_receive.ts
@@ -1,72 +1,77 @@
-import { QueriesClient, Config, Utils } from '../../../src';
+import { Config, Utils, QueriesClient } from '../../../src';
 
-function main() {
+async function main() {
   const opts: Config = {
     address: 'localhost:50000',
     clientId: 'asd',
   };
   const queriesClient = new QueriesClient(opts);
-  const subscriberA = queriesClient.subscribe({
-    channel: 'queries',
-    group: 'g1',
-    clientId: 'query-subscriberA',
-  });
-  const subscriberB = queriesClient.subscribe({
-    channel: 'queries',
-    group: 'g1',
-    clientId: 'query-subscriberB',
-  });
-
-  subscriberA.onQuery.on((query) => {
-    console.log('SubscriberA', query);
+  await queriesClient.subscribe(
+    {
+      channel: 'queries',
+      group: 'g1',
+      clientId: 'command-subscriberA',
+    },
+    (err, msg) => {
+      if (err) {
+        console.error('command-subscriberA', err);
+        return;
+      }
+      if (msg) {
+        console.log('SubscriberA', msg);
+        queriesClient
+          .response({
+            executed: true,
+            error: '',
+            replyChannel: msg.replyChannel,
+            clientId: 'command-responseA',
+            timestamp: Date.now(),
+            id: msg.id,
+            metadata: 'some getMetadata from response A',
+            body: Utils.stringToBytes('A says hi'),
+          })
+          .catch((reason) => console.log(reason));
+      }
+    },
+  );
+  await queriesClient.subscribe(
+    {
+      channel: 'queries',
+      group: 'g1',
+      clientId: 'command-subscriberB',
+    },
+    (err, msg) => {
+      if (err) {
+        console.error('command-subscriberB', err);
+        return;
+      }
+      if (msg) {
+        console.log('SubscriberB', msg);
+        queriesClient
+          .response({
+            executed: true,
+            error: '',
+            replyChannel: msg.replyChannel,
+            clientId: 'command-responseB',
+            timestamp: Date.now(),
+            id: msg.id,
+            metadata: 'some getMetadata from response B',
+            body: Utils.stringToBytes('B says, Im here too'),
+          })
+          .catch((reason) => console.log(reason));
+      }
+    },
+  );
+  await new Promise((r) => setTimeout(r, 2000));
+  for (let i = 0; i < 10; i++) {
     queriesClient
-      .response({
-        executed: true,
-        error: '',
-        replyChannel: query.replyChannel,
-        clientId: 'query-responseA',
-        timestamp: Date.now(),
-        id: query.id,
-        metadata: 'some getMetadata from response A',
-        body: Utils.stringToBytes('A says hi'),
+      .send({
+        channel: 'queries',
+        body: Utils.stringToBytes('data'),
+        timeout: 10000,
+        clientId: 'command-sender',
       })
-      .catch((reason) => console.log(reason));
-  });
-  subscriberA.onError.on((error) => console.error('SubscriberA', error));
-  subscriberA.onStateChanged.on((state) => console.log('SubscriberA', state));
-
-  subscriberB.onQuery.on((query) => {
-    console.log('SubscriberB', query);
-    queriesClient
-      .response({
-        executed: true,
-        error: '',
-        replyChannel: query.replyChannel,
-        clientId: 'query-responseB',
-        timestamp: Date.now(),
-        id: query.id,
-        metadata: 'some getMetadata from response B',
-        body: Utils.stringToBytes('B says, Im here too'),
-      })
-      .catch((reason) => console.log(reason));
-  });
-  subscriberB.onError.on((error) => console.error('SubscriberB', error));
-  subscriberB.onStateChanged.on((state) => console.log('SubscriberB', state));
-
-  setTimeout(() => {
-    for (let i = 0; i < 20; i++) {
-      queriesClient
-        .send({
-          channel: 'queries',
-          body: Utils.stringToBytes('data'),
-          timeout: 10000,
-          clientId: 'query-sender',
-        })
-        .catch((reason) => console.error(reason));
-    }
-  }, 2000);
-  setTimeout(() => {
-    queriesClient.close();
-  }, 4000);
+      .catch((reason) => console.error(reason));
+  }
 }
 main();
