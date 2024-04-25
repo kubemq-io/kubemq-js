fc3ae8b583924237dbfabbabaa5418cc75ef7297
KubeMQ <lior.nabat@kubemq.io>
2024-04-25 16:45:33 +0300

    Add list functionality to various clients
    This commit introduces list functionality that fetches 'channels' from different clients such as EventsStoreClient, EventsClient, QueuesClient, QueriesClient, and CommandsClient. Each client is set-up with a configuration, and an asynchronous list function that queries and logs the 'channels'.

---
+++ examples/queues/list.ts
@@ -0,0 +1,18 @@
+import { Config, Utils, QueuesClient } from '../../src';
+
+const opts: Config = {
+  address: 'localhost:50000',
+  clientId: Utils.uuid(),
+  reconnectInterval: 1000,
+};
+const queuesClient = new QueuesClient(opts);
+async function list(search: string) {
+  const channels = await queuesClient.list(search);
+  console.log(channels);
+}
+
+async function main() {
+  await list('');
+  // wait for receiver
+}
+main();
