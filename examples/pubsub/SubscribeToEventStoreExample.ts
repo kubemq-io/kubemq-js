83d40574aca90c1c4f131b2bd7685c8a3edacfa1
kkscwcd <kkscwcd@gmail.com>
2024-09-12 14:11:49 +0530

    Improved example and README.md file

--- examples/pubsub/SubscribeToEventStoreExample.ts
+++ examples/pubsub/SubscribeToEventStoreExample.ts
@@ -1,13 +1,13 @@
 import { Config, PubsubClient, EventStoreType, Utils, EventsStoreSubscriptionRequest, EventStoreMessageReceived } from '../../src';
 
-async function main() {
-  const opts: Config = {
-    address: 'localhost:50000',
-    clientId: 'kubeMQClientId-ts',
-    reconnectInterval: 2
-  };
-  const pubsubClient = new PubsubClient(opts);
+const opts: Config = {
+  address: 'localhost:50000',
+  clientId: 'kubeMQClientId-ts',
+  reconnectInterval: 2
+};
+const pubsubClient = new PubsubClient(opts);
 
+async function subscribeToEventStore() {
   //Subscribes to events store messages from the specified channel with a specific configuration.
   const eventsSubscriptionRequest = new EventsStoreSubscriptionRequest('events_store.A', '');
   eventsSubscriptionRequest.eventsStoreType = EventStoreType.StartAtSequence;
@@ -15,30 +15,35 @@ async function main() {
 
   // Define the callback for receiving events
   eventsSubscriptionRequest.onReceiveEventCallback = (event: EventStoreMessageReceived) => {
-      console.log('SubscriberA received event:', {
-          id: event.id,
-          fromClientId: event.fromClientId,
-          timestamp: event.timestamp,
-          channel: event.channel,
-          metadata: event.metadata,
-          body: event.body,
-          tags: event.tags,
-          sequence: event.sequence,
-      });
+    console.log('SubscriberA received event:', {
+      id: event.id,
+      fromClientId: event.fromClientId,
+      timestamp: event.timestamp,
+      channel: event.channel,
+      metadata: event.metadata,
+      body: event.body,
+      tags: event.tags,
+      sequence: event.sequence,
+    });
   };
-  
+
   // Define the callback for handling errors
   eventsSubscriptionRequest.onErrorCallback = (error: string) => {
-      console.error('SubscriberA error:', error);
+    console.error('SubscriberA error:', error);
   };
-  
+
   pubsubClient
-      .subscribeToEvents(eventsSubscriptionRequest)
-      .then(() => {
-          console.log('Eventstore Subscription successful');
-      })
-      .catch((reason: any) => {
-          console.error('Eventstore Subscription failed:', reason);
-      });
+    .subscribeToEvents(eventsSubscriptionRequest)
+    .then(() => {
+      console.log('Eventstore Subscription successful');
+    })
+    .catch((reason: any) => {
+      console.error('Eventstore Subscription failed:', reason);
+    });
 }
+
+async function main() {
+ await subscribeToEventStore();
+}
+
 main();
