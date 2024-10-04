c18bf00c2693c896fcee4fbe72f7e1f929de7e45
Lior Nabat <lior.nabat@gmail.com>
2024-10-04 16:21:43 +0300

    update v2.0.1

--- examples/queues/Send_ReceiveMessageExample.ts
+++ examples/queues/Send_ReceiveMessageExample.ts
@@ -1,4 +1,4 @@
-import { Config, QueuesClient, QueuesPollRequest, Utils } from 'kubemq-js'
+import { Config, QueuesClient, QueuesPollRequest, Utils } from 'kubemq-js';
 
 async function main() {
   const opts: Config = {
@@ -6,48 +6,52 @@ async function main() {
     clientId: 'kubeMQClientId-ts',
   };
   const queuesClient = new QueuesClient(opts);
-  
+
   //Send to single channel
   await queuesClient
     .sendQueuesMessage({
       channel: 'queues.single',
       body: Utils.stringToBytes('queue message'),
-      policy: {expirationSeconds:3600, delaySeconds:1, maxReceiveCount:3, maxReceiveQueue: 'dlq-queues.single'},
+      policy: {
+        expirationSeconds: 3600,
+        delaySeconds: 1,
+        maxReceiveCount: 3,
+        maxReceiveQueue: 'dlq-queues.single',
+      },
     })
     .then((result) => console.log(result))
     .catch((reason) => console.error(reason));
 
-   //Receive Queue Messagece
-const pollRequest = new QueuesPollRequest({
-  channel: 'queues.single',
-  pollMaxMessages: 1, // Maps to maxNumberOfMessages
-  pollWaitTimeoutInSeconds: 10, // Maps to waitTimeoutSeconds
-  autoAckMessages: false // Optional: add based on your needs
-});
-
-// Use the properties of QueuesPollRequest in the receiveQueuesMessages function
-await queuesClient
-  .receiveQueuesMessages(pollRequest)
-  .then((response) => {
-    console.log(response);
-    response.messages.forEach((msg) => {
-      console.log(msg);
-      // Message handling options:
-
-      // 1. Acknowledge message (mark as processed)
-      // msg.ack();
-
-      // 2. Reject message (won't be requeued)
-      // msg.reject();
-
-      // 3. Requeue message (send back to queue)
-      // msg.reQueue(channelName);
-    });
-  })
-  .catch((reason) => {
-    console.error(reason);
+  //Receive Queue Message
+  const pollRequest = new QueuesPollRequest({
+    channel: 'queues.single',
+    pollMaxMessages: 1, // Maps to maxNumberOfMessages
+    pollWaitTimeoutInSeconds: 10, // Maps to waitTimeoutSeconds
+    autoAckMessages: false, // Optional: add based on your needs
   });
-   
+
+  // Use the properties of QueuesPollRequest in the receiveQueuesMessages function
+  await queuesClient
+    .receiveQueuesMessages(pollRequest)
+    .then((response) => {
+      console.log(response);
+      response.messages.forEach((msg) => {
+        console.log(msg);
+        // Message handling options:
+
+        // 1. Acknowledge message (mark as processed)
+        // msg.ack();
+
+        // 2. Reject message (won't be requeued)
+        // msg.reject();
+
+        // 3. Requeue message (send back to queue)
+        // msg.reQueue(channelName);
+      });
+    })
+    .catch((reason) => {
+      console.error(reason);
+    });
 }
 
 main();
